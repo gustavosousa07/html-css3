68b80d1b8f50d8be3a9eda8c2b37cbdec0ac138d
gustavosousa07 <138171453+gustavosousa07@users.noreply.github.com>
2023-09-18 16:27:22 -0300

    criei o form005

--- exercicios/ex025/cadastro.php
+++ exercicios/ex025/cadastro.php
@@ -1,2 +1,21 @@
-Os dados foram enviados!
-Aprenda PHP para saber o que fazer com eles.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    $inome= "Gica";
+      if (inome="Gica") {
+        alert ("Acesso liberado");  
+    }
+    else{
+        alert ("Acesso negado");
+    }
+    ?>
+</body>
+</html>
+
